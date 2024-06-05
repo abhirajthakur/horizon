d409e93af8a1f4beb6f56036f7ccc5c84fbaaf93
Abhiraj Thakur <abhirajthakur124@gmail.com>
2024-06-05 16:17:46 +0530

    change currency to usd

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -3,7 +3,7 @@ import CountUp from "react-countup";
 const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
     <div className="w-full">
-      <CountUp end={amount} prefix="₹" decimals={2} />
+      <CountUp end={amount} prefix="$" decimals={2} />
     </div>
   );
 };
